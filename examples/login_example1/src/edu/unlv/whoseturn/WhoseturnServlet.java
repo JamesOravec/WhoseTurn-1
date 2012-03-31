34810355e76b4f382143174004e48dc27fd2a1ef
Ernesto Pavon <p3rseus@msn.com>
2012-03-31 15:31:59 -0700

    log-in example recognizes users

--- examples/login_example1/src/edu/unlv/whoseturn/WhoseturnServlet.java
+++ examples/login_example1/src/edu/unlv/whoseturn/WhoseturnServlet.java
@@ -3,6 +3,10 @@ package edu.unlv.whoseturn;
 import java.io.IOException;
 import javax.servlet.http.*;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 @SuppressWarnings("serial")
 public class WhoseturnServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -13,8 +17,18 @@ public class WhoseturnServlet extends HttpServlet {
 	
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/html");
-		resp.getWriter().println("<b>Hello World</b>");
+		
+		UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser(); // or req.getUserPrincipal()
+        
+        resp.setContentType("text/html");
+        if(user != null) {
+			
+			resp.getWriter().println("<b>Hello </b>" + user.getNickname());
+        }
+        else {
+        	resp.getWriter().println("<b>Hello anonymous</b>");
+        }
 	
 	}
 }
